110ec258035bd4a1040a213fa3757c35cf722bb0
barrozo1 <vinicius.barrozo@convenio.uece.br>
2021-10-19 19:28:46 -0300

    Create Pessoa.py
    Desafio 2

---
+++ Pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+    def __init__(self, nome, idade, sexo, cidade, estado):
+        self.Nome = nome
+        self.Idade = idade
+        self.Sexo = sexo
+        self.Cidade = cidade
+        self.Estado = estado
+
+
+pessoa = Pessoa('Marcus', 21, 'Masculino', 'Fortaleza', 'Ceará')
