1117fd7ef76baa7cac9ae5b83a347865d0f6323b
barrozo1 <vinicius.barrozo@convenio.uece.br>
2021-10-19 19:28:21 -0300

    Create main.py
    Desafio 1 e 3

---
+++ main.py
@@ -0,0 +1,19 @@
+""" Desafio 1 e Desafio 3"""
+
+
+def dados():
+    nome = input("Informe seu Nome: ")
+    idade = input("Informe sua idade (apenas números): ")
+    sexo = input("Informe seu sexo: ")
+    cidade = input("Informe a sua cidade: ")
+    estado = input("Informe o seu estado: ")
+    return nome, idade, sexo, cidade, estado
+
+
+pessoa = dados()
+
+print("Olá " + pessoa[0] + "!")
+print(
+      "Você tem " + pessoa[1] + " anos, pertence ao sexo " + pessoa[2] + ", reside em " + pessoa[3] + ", estado de(o) "
+      + pessoa[4] + "."
+     )
