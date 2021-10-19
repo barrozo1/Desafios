c593567f859104a79a7b07941f974c245da1dabd
barrozo1 <vinicius.barrozo@convenio.uece.br>
2021-10-19 20:10:19 -0300

    Desafio 1 e 3

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """ Desafio 1 e Desafio 3"""
 
+from Pessoa import *
+
 
 def dados():
     nome = input("Informe seu Nome: ")
@@ -8,11 +10,16 @@ def dados():
     cidade = input("Informe a sua cidade: ")
     estado = input("Informe o seu estado: ")
     return nome, idade, sexo, cidade, estado
+print(dados())
 
 
-pessoa = dados()
+pessoa = Pessoa(input("Informe seu Nome: "),
+                input("Informe sua idade (apenas números): "),
+                input("Informe seu sexo: "),
+                input("Informe a sua cidade: "),
+                input("Informe o seu estado: "))
 
-print("Olá " + pessoa[0] + "!")
+print("Olá " + pessoa.Nome + "!")
 print(
-      "Você tem " + pessoa[1] + " anos, pertence ao sexo " + pessoa[2] + ", reside em " + pessoa[3] + ", estado de(o) "
-      + pessoa[4] + ".")
+    "Você tem " + pessoa.Idade + " anos, pertence ao sexo " + pessoa.Sexo + ", reside em " + pessoa.Cidade + ", estado de(o) "
+    + pessoa.Estado + ".")
